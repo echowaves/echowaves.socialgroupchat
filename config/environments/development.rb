e1a8d2974967639f7793fe34ffb9fee3a88f90f1
Claudio Perez Gamayo <crossblaim@gmail.com>
2010-02-20 23:07:42 +0100

    use devise for authentication, require mongo_ext, setup some default values for devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,4 +16,5 @@ Echowaves::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Echowaves::Application.routes.draw do |map|
+
+  root :to => "home#index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -52,7 +55,4 @@ Echowaves::Application.routes.draw do |map|
 
   # See how all your routes lay out with "rake routes"
 
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
 end
