5bed291cb8344898f63c5fbf9d27afe09c8dd75a
dmitryame <dmitry@custommode.com>
2010-03-21 23:54:11 -0400

    mongo configuration

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Echowaves::Application.initialize!
+
+
+config.frameworks -= [ :active_record ] # this line is usually commented out, uncomment it and change
