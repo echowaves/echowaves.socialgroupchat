af889fb5cdc6ec3cbb71d128e15f6e29821b9af6
dmitryame <dmitry@custommode.com>
2010-03-29 23:05:41 -0400

    filling up the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,28 @@ class User
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation
+
+
+
+  field :email #  t.string,  :null               => false
+  field :encrypted_password # t.string, :limit   => 40, :null => false
+  field :password_salt # t.string, :null         => false
+  field :confirmation_token # t.string, :limit   => 20
+  field :confirmed_at # t.datetime 
+  field :confirmation_sent_at # t.datetime 
+  field :reset_password_token # t.string, :limit => 20
+  field :remember_token # t.string, :limit       => 20
+  field :remember_created_at # t.datetime 
+  field :sign_in_count # t.integer  
+  field :current_sign_in_at # t.datetime 
+  field :last_sign_in_at # t.datetime 
+  field :current_sign_in_ip # t.string   
+  field :last_sign_in_ip # t.string   
+  field :failed_attempts # t.integer, :default   => 0
+  field :unlock_token # t.string, :limit         => 20
+  field :locked_at # t.datetime 
+  field :created_at # t.datetime 
+  field :updated_at # t.datetime 
+
+
 end
