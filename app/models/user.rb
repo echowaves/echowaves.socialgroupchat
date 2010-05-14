d5dcf1739c207250eb540974762ba10d473317dd
Claudio Perez Gamayo <crossblaim@gmail.com>
2010-05-14 21:32:16 +0200

    fix user model. install dynamic_form plugin so we can see the error messages for the forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,21 +10,21 @@ class User
   field :encrypted_password # t.string, :limit   => 40, :null => false
   field :password_salt # t.string, :null         => false
   field :confirmation_token # t.string, :limit   => 20
-  field :confirmed_at # t.datetime
-  field :confirmation_sent_at # t.datetime
+  field :confirmed_at, :type => DateTime # t.datetime
+  field :confirmation_sent_at, :type => DateTime # t.datetime
   field :reset_password_token # t.string, :limit => 20
   field :remember_token # t.string, :limit       => 20
-  field :remember_created_at # t.datetime
-  field :sign_in_count # t.integer
-  field :current_sign_in_at # t.datetime
-  field :last_sign_in_at # t.datetime
+  field :remember_created_at, :type => DateTime # t.datetime
+  field :sign_in_count, :type => Integer # t.integer
+  field :current_sign_in_at, :type => DateTime # t.datetime
+  field :last_sign_in_at, :type => DateTime # t.datetime
   field :current_sign_in_ip # t.string
   field :last_sign_in_ip # t.string
-  field :failed_attempts # t.integer, :default   => 0
+  field :failed_attempts, :type => Integer # t.integer, :default   => 0
   field :unlock_token # t.string, :limit         => 20
-  field :locked_at # t.datetime
-  field :created_at # t.datetime
-  field :updated_at # t.datetime
+  field :locked_at, :type => DateTime # t.datetime
+  field :created_at, :type => DateTime # t.datetime
+  field :updated_at, :type => DateTime # t.datetime
 
 
 end
