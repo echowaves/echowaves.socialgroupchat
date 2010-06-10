7f5e19e2bd74c11a3cda575b5bb43472ff9002d9
dmitryame <dmitry@custommode.com>
2010-06-10 11:27:18 -0400

    added validation

--- app/models/convo.rb
+++ app/models/convo.rb
@@ -1,5 +1,9 @@
 class Convo
   include Mongoid::Document
+  validates_presence_of :title
+  validates_length_of :title, :maximum => 140
+  
+  
   field :title, :type => String
   embeds_one :user
 end
