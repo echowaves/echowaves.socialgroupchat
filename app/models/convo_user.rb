b5f977751b2efc9ab50e917dc3dc17999bd5a945
dmitryame <dmitry@custommode.com>
2010-07-30 20:06:41 -0400

    cpmment added

--- app/models/convo_user.rb
+++ app/models/convo_user.rb
@@ -1,3 +1,4 @@
+# this model represents a many-to-many relationship between convos and users, it stores the embedded objects for performance
 class ConvoUser
   include Mongoid::Document
   validates_presence_of :convo
