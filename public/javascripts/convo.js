b23809d74039c3a2fbf65aea745d252a8fca5ae6
Claudio Perez Gamayo <crossblaim@gmail.com>
2010-08-25 00:00:04 +0200

    scroll convo to bottom

--- public/javascripts/convo.js
+++ public/javascripts/convo.js
@@ -1,5 +1,11 @@
 $(function(){
 
+    // scroll the convo to the bottom
+    scrollBottom = function(){
+        $("#convo_content").attr({ scrollTop: $("#convo_content").attr("scrollHeight") - $('#convo_content').height() }, 3000);
+    };
+
+    // draw a message
     messageView = function(container, message, template){
         msg = template.clone().attr('id',message.uuid);
         var draw = function(){
