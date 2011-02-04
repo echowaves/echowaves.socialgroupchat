72b043e916ee3ca37bf2efdbb93d30b4b8d5d00f
dmitryame <dmitry@custommode.com>
2011-02-04 18:17:06 -0500

    subscriptions cuke is steakified now

--- spec/acceptance/subscriptions_spec.rb
+++ spec/acceptance/subscriptions_spec.rb
@@ -8,14 +8,37 @@ feature "Subscriptions", %q{
 
 
   background do
-    @user = active_user
-    login_as_user @user
+    @user = login_new
   end
 
 
-  scenario "registered user creates a public/private Convo which results in subscribing to that convo as well" do
+  scenario "registered user creates a public Convo which results in subscribing to that convo as well" do
     @convo = Convo.make(:user => @user, :title => "my new convo", :privacy => "public")
     visit user_subscriptions_path @user
-    page.should containt_content"my new convo"
+    page.should have_content"my new convo"
   end
+
+
+  scenario "registered user creates a private Convo which results in subscribing to that convo as well" do
+    @convo = Convo.make(:user => @user, :title => "my new convo", :privacy => "private")
+    visit user_subscriptions_path @user
+    page.should have_content"my new convo"
+  end
+
+
+  scenario "test subscriptions pagination" do
+    21.times do |i|
+      Convo.make(:user => @user, :title => "Convo #{i}.", :created_at => i*1000)
+    end
+    visit user_subscriptions_path @user
+    page.should have_content"Convo 20."
+    page.should have_content"Next"
+    page.should have_content"Previous"
+    page.should have_no_content"Convo 0."
+    click_link "Next"
+    page.should have_no_content"Convo 20."
+    page.should have_content"Convo 0."
+  end
+  
+  
 end
