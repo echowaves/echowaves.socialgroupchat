85bdcc203ec7d83c4a507fa92b3a41a16b7992d7
dmitryame <dmitry@custommode.com>
2011-02-08 21:49:04 -0500

    removed the socky plugin (using gem instead), adjusted the default config to run on localhost for testing purposes

--- vendor/plugins/socky_plugin/Rakefile
+++
@@ -1,9 +0,0 @@
-require 'rake'
-require 'spec/rake/spectask'
-
-task :default => :spec
-
-Spec::Rake::SpecTask.new do |t|
-  t.ruby_opts = ['-rtest/unit']
-  t.spec_files = FileList['spec/**/*_spec.rb']
-end
--- vendor/plugins/socky_plugin/init.rb
+++
@@ -1,6 +0,0 @@
-require File.dirname(__FILE__) + '/lib/socky'
-require File.dirname(__FILE__) + '/lib/socky/helper'
-
-ActionView::Base.send(:include, Socky::Helper)
-
-ActionView::Helpers::AssetTagHelper.register_javascript_expansion :socky => ['socky/swfobject', 'socky/FABridge', 'socky/web_socket', 'socky/json2', 'socky']
