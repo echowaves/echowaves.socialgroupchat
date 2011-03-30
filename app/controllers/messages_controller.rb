08731c4ed0634e6422f6bb26b17ee740da3efafd
dmitryame <dmitry@custommode.com>
2011-03-30 19:46:12 -0400

    i like this method of finding by id better, cleaner

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -33,7 +33,7 @@ class MessagesController < ApplicationController
 private
 
   def current_convo
-    @convo = Convo.criteria.for_ids(params[:convo_id])[0]
+    @convo = Convo.find(params[:convo_id])
   end
 
 end
