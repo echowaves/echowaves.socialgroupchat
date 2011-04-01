57bc0e7d0d297b5fde7de1ddd1d02634994929f7
dmitryame <dmitry@custommode.com>
2011-04-01 19:57:01 -0400

    helper covered 100%

---
+++ spec/helpers/convos_helper_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe ConvosHelper do
+  describe "#subscribe_or_unsubscribe_link(:convo, :user)" do
+    it "creates subscribe link if the user is not subscribed" do
+      user = User.make
+      convo = Convo.make!
+      content = helper.subscribe_or_unsubscribe_link(:convo => convo, :user => user)
+      content.should include ">subscribe<"
+    end
+  end 
+  it "creates unsubscribe link if the user is subscribed" do
+    user = User.make!
+    convo = Convo.make!
+    convo.add_user user
+    content = helper.subscribe_or_unsubscribe_link(:convo => convo, :user => user)
+    content.should include ">unsubscribe<"
+  end
+end
