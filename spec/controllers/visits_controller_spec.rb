b79573ee175b3f7e4565a4bede029248bb19f611
dmitryame <dmitry@custommode.com>
2011-04-09 16:24:18 -0400

    covering the visits controller

---
+++ spec/controllers/visits_controller_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe VisitsController do
+  let(:convo) { mock_model(Convo).as_null_object }  
+
+  before do
+    @user = User.make
+    request.env['warden'] = mock(Warden, :authenticate => @user, :authenticate! => @user, :authenticate? => @user)
+    @user.stub(:visited_convos).and_return([convo])
+  end
+
+  describe "GET index" do
+    it "assigns @convos with only visited convos" do
+      get :index
+
+      assigns(:convos).should eq([convo])
+      response.should render_template(:index)
+    end
+  end
+  
+end
