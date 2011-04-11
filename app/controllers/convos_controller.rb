475091047b5ed103ef3ae3e727684536a00bf644
dmitryame <dmitry@custommode.com>
2011-04-11 18:51:24 -0400

    using mongo scopes in models

--- app/controllers/convos_controller.rb
+++ app/controllers/convos_controller.rb
@@ -5,7 +5,7 @@ class ConvosController < ApplicationController
 
   def index
     # here we list only public convos
-    @convos = Convo.where(:privacy => "public").desc(:created_at).page(params[:page])
+    @convos = Convo.public.by_date.page(params[:page])
     respond_with @convos
   end
 
--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -4,7 +4,6 @@ class SubscriptionsController < ApplicationController
 
   def index
     @user_id = params[:user_id]    
-    # @convos = Convo.desc(:created_at).where(:privacy => "public").page(params[:page])
     # FIXME: n+1 query!!!!!!!!!!!!!!!!!!!!!!!!    
     # potentially might be able to retreive all the convo_id from @ssubscriptions collection, then find all the @convos in one queries -- still more then one query (2), but much better then n+1    
     @subscriptions = Subscription.desc(:created_at).where(:user_id => @user_id).page(params[:page])
