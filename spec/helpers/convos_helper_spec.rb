20989f4bb0e9a36ab68208dcffefe973dd8ba06a
dmitry <dmitry@echowaves.com>
2011-05-10 19:53:51 -0400

    subscribe/unsubscribe helper test passes

--- spec/helpers/convos_helper_spec.rb
+++ spec/helpers/convos_helper_spec.rb
@@ -1,19 +1,20 @@
 require 'spec_helper'
 
 describe ConvosHelper do
+  before do
+    @user = Factory(:user)
+    @convo = Factory(:convo, :privacy_level => 1) #public convo
+  end
+  
   describe "#subscribe_or_unsubscribe_link(:convo, :user)" do
     it "creates subscribe link if the user is not subscribed" do
-      user = User.make
-      convo = Convo.make!
-      content = helper.subscribe_or_unsubscribe_link(:convo => convo, :user => user)
+      content = helper.subscribe_or_unsubscribe_link(:convo => @convo, :user => @user)
       content.should include ">subscribe<"
     end
   end 
   it "creates unsubscribe link if the user is subscribed" do
-    user = User.make!
-    convo = Convo.make!
-    convo.subscribe user
-    content = helper.subscribe_or_unsubscribe_link(:convo => convo, :user => user)
+    @convo.subscribe @user
+    content = helper.subscribe_or_unsubscribe_link(:convo => @convo, :user => @user)
     content.should include ">unsubscribe<"
   end
 end
