32d42d6928ef03faf4ebcbc2f435d93e1040cf0f
dmitryame <dmitry@custommode.com>
2011-05-10 22:34:22 -0400

    EscapeUtils is no longer needed with latest rack

--- config/initializers/fixes.rb
+++ config/initializers/fixes.rb
@@ -1,10 +1,10 @@
 # fix ~/.rvm/gems/ruby-1.9.2-head/gems/rack-1.2.1/lib/rack/utils.rb:16: warning: regexp match /.../n against to UTF-8 string
 # uses the escape_utils gem (http://github.com/brianmario/escape_utils)
-module Rack
-  module Utils
-    def escape(s)
-      EscapeUtils.escape_url(s)
-    end
-  end
-end
-
+# module Rack
+#   module Utils
+#     def escape(s)
+#       EscapeUtils.escape_url(s)
+#     end
+#   end
+# end
+# 
