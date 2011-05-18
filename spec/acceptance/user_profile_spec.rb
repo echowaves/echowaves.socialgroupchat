1af569babd3ce86654317fba862700dd991efe94
dmitry <dmitry@echowaves.com>
2011-05-18 08:44:43 -0400

    fixing acceptance specs

--- spec/acceptance/user_profile_spec.rb
+++ spec/acceptance/user_profile_spec.rb
@@ -8,11 +8,12 @@ feature "User Profile", %q{
 
 
   background do
-    @user = login_new
+    @user = active_user
   end
 
 
   scenario "Own profile" do
+    login_as_user @user
     visit "/"
     click_link "nav_profile_link"
     page.should have_content"#{@user.username}'s profile"    
@@ -21,6 +22,7 @@ feature "User Profile", %q{
   
   scenario "Social profile" do
     @user2 = active_user
+    login_as_user @user2
     visit users_path
     click_link @user2.username
     page.should have_content"#{@user2.username}'s profile"    
@@ -29,11 +31,21 @@ feature "User Profile", %q{
 
   scenario "test a user follow/unfollow from a profile page stays on the same page" do
     @user2 = active_user
+    login_as_user @user2
     visit user_path @user2
 
     click_link "follow"
     click_link "unfollow"
     current_path.should == user_path(@user2)
+  end
+
+  scenario "trying to access not own profile" do
+    @user2 = active_user
+    login_as_user @user
+    visit users_path
+    click_link @user2.username
+    page.should have_content"This is not your profile, you can\'t see it."    
+    current_path.should == users_path
   end
 
   
