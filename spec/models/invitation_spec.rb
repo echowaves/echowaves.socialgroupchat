f60363ee8bca8780813951c024713d514df0262d
dmitryame <dmitry@custommode.com>
2011-06-04 10:55:29 -0400

    initation_spec green

--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -9,31 +9,31 @@ describe Invitation do
 
     # columns
     #----------------------------------------------------------------------
-    it { should have_column :convo_id, :type  => :integer, :null => false }
-    it { should have_column :user_id, :type  => :integer, :null => false }
-    it { should have_column :requestor_id, :type  => :integer, :null => false }
-    it { should have_column :created_at, :type => :datetime }
-    it { should have_column :updated_at, :type => :datetime }
+    it { should have_db_column(:convo_id).of_type(:integer).with_options(:null => false) }
+    it { should have_db_column(:user_id).of_type(:integer).with_options(:null => false) }
+    it { should have_db_column(:requestor_id).of_type(:integer).with_options(:null => false) }
+    it { should have_db_column(:created_at).of_type(:datetime) }
+    it { should have_db_column(:updated_at).of_type(:datetime) }
 
     # indexes
     #----------------------------------------------------------------------
-    it { should have_index :convo_id }
-    it { should have_index :user_id }
-    it { should have_index :requestor_id }
-    it { should have_index :created_at }
+    it { should have_db_index :convo_id }
+    it { should have_db_index :user_id }
+    it { should have_db_index :requestor_id }
+    it { should have_db_index :created_at }
 
     # validations
     #----------------------------------------------------------------------
     it { should validate_presence_of :convo }
     it { should validate_presence_of :user }
     it { should validate_presence_of :requestor }
-    it { should validate_uniqueness_of :convo_id, :scope => :user_id }
+    it { should validate_uniqueness_of(:convo_id).scoped_to(:user_id) }
 
     # associations
     #----------------------------------------------------------------------
-    # it { should belong_to :convo }
-    # it { should belong_to :user }
-    # it { should belong_to :requestor, class_name: "User" }
+    it { should belong_to :convo }
+    it { should belong_to :user }
+    it { should belong_to :requestor }
     #----------------------------------------------------------------------
 
   end
